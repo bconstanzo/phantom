e1925936a3e5efd3e43f65986b73802d4db0d924
bconstanzo <bconstanzo@users.noreply.github.com>
2018-11-11 12:03:52 -0300

    fixes to .gitignore and missing file
    .pstats and .dot added to .gitignore
    phantom/video.py was missing

---
+++ phantom/video.py
@@ -0,0 +1,8 @@
+"""
+Miscelanous utilities that allow to easily work with video.
+
+Mostly wraps OpenCVs classes and functions to give an easy interface. Mostly
+aimed at making the examples easier to understand.s
+"""
+
+# still a work in progress
