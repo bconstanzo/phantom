64937765824f1a7ef271b63b7cd7868f72d7fac3
bconstanzo <bconstanzo@users.noreply.github.com>
2018-11-17 19:54:37 -0300

    cleanup & incremental changes on tagger.py

--- tools/gender/tagger.py
+++ tools/gender/tagger.py
@@ -15,20 +15,23 @@ from phantom.utils import draw_faces
 
 
 # Here be ~dragons~ the part were you can tweak configs
-FLAG_TAG   = True
-FLAG_SAVE  = True
-FLAG_TRAIN = True
-FLAG_TEST  = True
-PATH_TRAIN = "D:/Storage-post-SSD/dlib_faces_5points/images"
-PATH_SAVE  = "./model.pickle"
-PATH_TEST  = ""
-CONST_FONT = cv2.FONT_HERSHEY_SIMPLEX
+FLAG_TAG     = False
+FLAG_SAVE    = False
+FLAG_TRAIN   = True
+FLAG_TEST    = True
+PATH_TRAIN   = "D:/Storage-post-SSD/dlib_faces_5points/images"
+PATH_TAGFILE = "./tagged_faces.csv"
+PATH_SVMFILE = "./model.pickle"
+PATH_TEST    = ""  # point to a directory were you can easily check!
+CONST_FONT   = cv2.FONT_HERSHEY_SIMPLEX
+
+# and that's it for constants, now a few variables
 tags_female = +1
 tags_male   = -1
 tagged = []
 color_cycle = cycle([
-    (  0, 255,   0),
     (255,   0,   0),
+    (  0, 255,   0),
     (  0,   0, 255),
     (255, 255,   0),
     (  0, 255, 255),
@@ -37,12 +40,36 @@ color_cycle = cycle([
 
 
 # Classes/namedtuples
-TaggedFace = namedtuple("TaggedFace", "gender_tag path img face")
+class TaggedFace:
+    """
+    Class to hold the results from the tagging.
+    
+    Formerly a named tuple, the __repr__ method was annoying on the console.
+
+    :param tag: the gender tag assigned
+    :param path: path to the image file
+    :param img: cv2/numpy array with the loaded image
+    :param face: list of facial landmarks in the image, result of
+        `phantom.faces.detect()`
+    """
+    def __init__(self, tag, path, img, faces):
+        self.tag = tag
+        self.path = path
+        self.img = img
+        self.faces = faces
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}(tag={self.tag}, path={self.path})"
 
 
 def tag():
     """
     Show images from a path waiting for user input to tag them.
+
+    We're keeping only the images were dlibs HOG face detector picks up only one
+    face. Using dlibs 5 point face landmark dataset a balanced female/male ratio
+    is found at about ~600 images. We also skip images were landmarking goes
+    wrong, as they can affect the result of facial encoding.
 
     :return: list of TaggedFace for each image
     """
@@ -51,7 +78,7 @@ def tag():
                 (img * 0.5)).astype(np.uint8)
         noface = img.copy()
         height, width = img.shape[:2]
-        if height > 960  or width > 1800:
+        if height > 960  or width > 1800:  # hardcoded and hacky, but works
             face   = cv2.resize(face, (int(width/2), int(height/2)))
             noface = cv2.resize(noface, (int(width/2), int(height/2)))
         
@@ -107,21 +134,37 @@ def tag():
     return tagged
 
 
-def load_tagged():
+def load_tagged(path):
     """
-    Loads the tagged image data from a file. WIP, for now it just returns
-    an empty list.
+    Loads the tagged image data from a CSV file.
 
     :return: empty list
     """
     return []
 
 
+def save_tagged(tagged, path):
+    """
+    Saves the tagged image data to a CSV file.
+
+    :param tagged: list of TaggedFace
+    :param path: file path to save to
+    """
+    header = "path,tag\n"
+    with open(path, "w") as filehandle:
+        filehandle.write(header)
+        for t in tagged:
+            filehandle.write(f"{t.path},{t.tag}\n")
+    return None
+
+
 def main():
     if FLAG_TAG:
         tagged = tag()
+        if FLAG_SAVE:
+            save_tagged(tagged, PATH_TAGFILE)
     else:
-        tagged = load_tagged()
+        tagged = load_tagged(PATH_TAGFILE)
 
 
 if __name__ == "__main__":
