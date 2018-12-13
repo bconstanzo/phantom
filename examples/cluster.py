0b765db902e849a992352cbb313ef84a570488bf
bconstanzo <bconstanzo@users.noreply.github.com>
2018-12-13 15:59:23 -0300

    WIP on cluster() function
    still ironing out issues with the new implementation that uses scikit-learn's DBSCAN and KMeans clustering algorithms

--- examples/cluster.py
+++ examples/cluster.py
@@ -10,15 +10,19 @@ import numpy as np
 import os
 
 
+from collections import defaultdict
 from concurrent.futures import ProcessPoolExecutor
 from phantom.faces import detect, encode
+from phantom.utils import image_grid
 from pprint import pprint
+from sklearn.cluster import DBSCAN, KMeans
 
 
-path  = r"D:\Storage-post-SSD\gender\test\small"
+path  = r"D:\Storage-post-SSD\Wapp\WhatsApp Images"
+path  = r"D:\Storage-post-SSD\gender\test\small"    # keeping the other path as a stress-test
 procs = 3
 DEBUG_TIMER = True
-output_folder_path = r"D:\Storage-post-SSD\gender\test\small_clustered"
+output_folder_path = r"D:\Storage-post-SSD\gender\cluster_test"
 
 
 def read_and_find(path):
@@ -36,20 +40,77 @@ def read_and_find(path):
     locations = detect(img)
     if not(locations):
         return tuple()
-    return img, encode(img, locations=locations), locations
+    return img, encode(img, locations=locations), locations, path
 
 
-def cluster(resultset)
+def cluster(resultset):
     """
     Works over the provided resultset to cluster the faces on it. resultset
     comes from `read_and_find`.
 
-    :param resultset: 
+    :param resultset: list of results (tuples) from `read_and_find`. Each tuple
+        has (cv2 image, list of encodings, list of face locations, path)
     """
-    images = [r[0] for r in resultset]
-    results = [dlib.vector(r[1][0]) for r in resultset]  # tied to 1 face per image...
-    labels = dlib.chinese_whispers_clustering(resultset, 0.5)
-    num_classes = len(set(labels))
+    images = []          # list of cv2 images
+    paths = []           # list of paths for each image
+    faces = []           # flat list of encodings
+    face_images = []     # list of cv2 images of each face found
+    images_x_faces = []  # this way we can zip the encodings to an image/path
+
+    for idx, packed in enumerate(resultset):
+        img, encodings, locations, path = packed
+        images.append(img)
+        paths.append(path)
+        for enc, loc in zip(encodings, locations):
+            faces.append(enc)
+            left, top, right, bottom = loc
+            roi = img[top:bottom, left:right]
+            face_images.append(roi)
+            images_x_faces.append(idx)
+    # the idea is simple, we apply DBSCAN with a basic configuration and use
+    # its result to apply k-means clustering:
+    print(f"Number of faces detected: {len(faces)}")
+    t0 = datetime.datetime.now()
+    db = DBSCAN(eps=0.475, min_samples=2).fit(faces)
+    t1 = datetime.datetime.now()
+    # we can now approximate how many people are present...
+    num_people = len(set(i for i in db.labels_ if i >= 0))
+    # ...and use k-means to identify all the labels that DBSCAN couldn't
+    km = KMeans(init="k-means++", n_clusters=num_people, n_init=10).fit(faces)
+    t2 = datetime.datetime.now()
+    # now we group all the images for each cluster into a grid
+    grid_images = defaultdict(list)
+    for idx, img, label in zip(range(len(face_images)), face_images, km.labels_):
+        print(f"Image: {paths[images_x_faces[idx]]}...")
+        if img is not None:
+            try:
+                grid_images[label].append(cv2.resize(img, (96, 96)))
+            except cv2.error:
+                pass
+    labels_set = set(km.labels_)
+    for label in labels_set:
+        # TODO: change this to a more flexible approach
+        if len(grid_images[label]) < 10:
+            grid_size = (3,3)
+        elif len(grid_images[label]) <= 25:
+            grid_size = (5,5)
+        elif len(grid_images[label]) <  50:
+            grid_size = (7,7)
+        elif len(grid_images[label]) <= 81:
+            grid_size = (9,9)
+        elif len(grid_images[label]) <= 121:
+            grid_size = (11,11)
+        elif len(grid_images[label]) <= 400:
+            grid_size = (20,20)
+        else:
+            grid_size = (30, 30)
+        out = image_grid(grid_images[label], grid_size, size=(96, 96))
+        cv2.imwrite(f"{output_folder_path}/grid_{label}.jpg", out)
+    t3 = datetime.datetime.now()
+    print(f"Number of people found: {num_people}")
+    print(f"DBSCAN took {t1 - t0}")
+    print(f"KMeans took {t2 - t1}")
+    print(f"image_grid() and saving took ")
     return None
 
 
@@ -68,14 +129,11 @@ def main():
     t1 = datetime.datetime.now()
     cluster(results)
     t2 = datetime.datetime.now()
-    
-    print("Number of clusters: {}".format(num_classes))
-    print("Saving faces to output folder...")
 
-    for i, label in enumerate(labels):
-        img = images[i]
-        file_path = os.path.join(output_folder_path, f"{label}_face_{i}.jpg")
-        cv2.imwrite(file_path, img)
+    # for i, label in enumerate(labels):
+    #     img = images[i]
+    #     file_path = os.path.join(output_folder_path, f"{label}_face_{i}.jpg")
+    #     cv2.imwrite(file_path, img)
 
     if DEBUG_TIMER:
         print(f"Time taken encoding  : {t1 - t0}")
