469d4ee5f9f7e15753c63a95aff47115969cd7b6
bconstanzo <bconstanzo@users.noreply.github.com>
2019-02-15 09:29:56 -0300

    added reference links to video.py

--- phantom/video.py
+++ phantom/video.py
@@ -6,3 +6,7 @@ aimed at making the examples easier to understand.s
 """
 
 # still a work in progress
+# consider reading:
+# * https://stackoverflow.com/questions/3431434/video-stabilization-with-opencv/47483926#47483926
+# * https://www.learnopencv.com/video-stabilization-using-point-feature-matching-in-opencv/?ck_subscriber_id=272167184
+# * Optical flow
