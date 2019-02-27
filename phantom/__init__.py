44642e99375098ac0b60da44dbbbaa57f773944c
bconstanzo <bconstanzo@users.noreply.github.com>
2019-02-27 10:04:40 -0300

    0.5 unstable announce
    start moving towards 0.6
    "unstable" means we're working towards stabilizing the namespaces and API of the project to 0.6.

--- phantom/__init__.py
+++ phantom/__init__.py
@@ -1,6 +1,12 @@
 import phantom.align
+import phantom.clustering
 import phantom.faces
+import phantom.geometry
 import phantom.utils
 import phantom.video
 
-__version__ = "0.4.0"
+__version__ = "0.5.0-unstable"
+# "unstable" here means we're working towards stabilizing the namespaces and API
+# of the project to 0.6. For every step towards reaching that goal, we'll update
+# the micro number until we can jump to 0.6 and everything has been ordered to
+# the new conventions.
