8c1af46c9c69f3db0ed26edd0d061716242d0bad
ayrton30 <42283019+ayrton30@users.noreply.github.com>
2019-04-08 12:29:22 -0300

    Update enhance.py

--- phantom/enhance.py
+++ phantom/enhance.py
@@ -8,6 +8,7 @@ import numpy as np
 # TO READ:
 # * http://onsignalandimageprocessing.blogspot.com/2017/02/lucy-richardson-deconvolution.html
 
+
 def lucy_richardson_deconv(img, num_iterations, sigmag):
     """" Lucy-Richardson Deconvolution Function
     // input-1 img: NxM matrix image
@@ -15,23 +16,20 @@ def lucy_richardson_deconv(img, num_iterations, sigmag):
     // input-3 sigma: sigma of point spread function (PSF)
     // output result: deconvolution result
     """
-	
-    EPSILON = 2.2204e-16
 
-    # Window size of PSF
-    win_size = 8 * sigmag + 1
+    epsilon = 2.2204e-16
+    win_size = 8 * sigmag + 1   # Window size of PSF
 
     # Initializations Numpy
-    J1 = img.copy()
-    J2 = img.copy()
-    wI = img.copy()
-    imR = img.copy()
+    j1 = img.copy()
+    j2 = img.copy()
+    w_i = img.copy()
+    im_r = img.copy()
 
-    # T1, T2, tmpMat1, tmpMat2 -> Numpy
-    T1 = np.zeros((np.size(img, 0), np.size(img, 1), 1), dtype="double")
-    T2 = np.zeros((np.size(img, 0), np.size(img, 1), 1), dtype="double")
-    tmpMat1 = np.zeros((np.size(img, 0), np.size(img, 1), 1), dtype="double")
-    tmpMat2 = np.zeros((np.size(img, 0), np.size(img, 1), 1), dtype="double")
+    t1 = np.zeros((np.size(img, 0), np.size(img, 1), 1), dtype="double")
+    t2 = np.zeros((np.size(img, 0), np.size(img, 1), 1), dtype="double")
+    tmp1 = np.zeros((np.size(img, 0), np.size(img, 1), 1), dtype="double")
+    tmp2 = np.zeros((np.size(img, 0), np.size(img, 1), 1), dtype="double")
     # size = (w, h, channels), grayscale -> channels = 1
 
     # Lucy - Rich.Deconvolution CORE
@@ -40,61 +38,34 @@ def lucy_richardson_deconv(img, num_iterations, sigmag):
         if j > 1:
             # calculation of lambda
             # https://docs.opencv.org/2.4/modules/core/doc/operations_on_arrays.html#multiply
-            cv2.multiply(T1, T2, tmpMat1)   # Calculates the per-element scaled product of two arrays.
-            cv2.multiply(T2, T2, tmpMat2)
+            cv2.multiply(t1, t2, tmp1)   # Calculates the per-element scaled product of two arrays.
+            cv2.multiply(t2, t2, tmp2)
 
             # https://docs.opencv.org/2.4/modules/core/doc/operations_on_arrays.html#sum
-            lambda_ = cv2.sumElems(tmpMat1)[0] / (cv2.sumElems(tmpMat2)[0] + EPSILON)
-            print("lambda [", j, "]", lambda_)
+            lambda_ = cv2.sumElems(tmp1)[0] / (cv2.sumElems(tmp2)[0] + epsilon)
 
-        # Y = J1 + (lambda_ * (J1 - J2))
-        resta = np.subtract(J1, J2)
-        multi = np.multiply(lambda_, resta)
-        Y = J1 + multi
+        # y = j1 + (lambda_ * (j1 - j2))
+        #subtract =
+        multiply = np.multiply(lambda_, np.subtract(j1, j2))
+        y = j1 + multiply
 
-        Y[(Y < 0)] = 0
+        y[(y < 0)] = 0
 
-        # 1) applying Gaussian filter
-        reBlurred = cv2.GaussianBlur(Y, (int(win_size), int(win_size)), sigmag)
-        reBlurred[(reBlurred <= 0)] = EPSILON
+        # applying Gaussian filter
+        re_blurred = cv2.GaussianBlur(y, (int(win_size), int(win_size)), sigmag)
+        re_blurred[(re_blurred <= 0)] = epsilon
 
-        # 2)
-        cv2.divide(wI, reBlurred, imR)
-        imR = imR + EPSILON
+        cv2.divide(w_i, re_blurred, im_r)
+        im_r = im_r + epsilon
 
-        # 3) applying Gaussian filter
-        imR = cv2.GaussianBlur(imR, (int(win_size), int(win_size)), sigmag)
+        # applying Gaussian filter
+        im_r = cv2.GaussianBlur(im_r, (int(win_size), int(win_size)), sigmag)
 
-        # 4)
-        J2 = J1.copy()
-        cv2.multiply(Y, imR, J1)
+        j2 = j1.copy()
+        cv2.multiply(y, im_r, j1)
 
-        T2 = T1.copy()
-        T1 = J1 - Y
+        t2 = t1.copy()
+        t1 = j1 - y
 
-    result = J1.copy()
+    result = j1.copy()
     return result
-
-# example
-# sigmaG: sigma of point spread function
-sigma = 6.0
-winSize = 8 * sigma + 1
-
-img = cv2.imread('lena.png', cv2.IMREAD_GRAYSCALE)    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow('original', img)
-
-img = np.float64(img)   # convert to -> img.convertTo(img, CV_64F);
-
-# Blur the original image
-img = cv2.GaussianBlur(img, (int(winSize), int(winSize)), sigma, sigma)
-# en C++ ->GaussianBlur(img_origen, img_destino, Size(winSize,winSize), sigmaG, sigmaG )
-
-cv2.normalize(img, img, 0, 1, cv2.NORM_MINMAX)
-cv2.imshow('original_blur', img)
-
-result = lucy_richardson_deconv(img, 200, sigma)
-cv2.normalize(result, result, 0, 1, cv2.NORM_MINMAX)
-
-cv2.imshow('result', result)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
