1be5b508317884e2962b63999e17099f1d5621ca
bconstanzo <bconstanzo@users.noreply.github.com>
2019-10-08 22:26:43 -0300

    feat: added measures submodule

---
+++ phantom/measures.py
@@ -0,0 +1,11 @@
+"""
+Functions and algorithms related to measuring things in images.
+"""
+import cv2
+import numpy as np
+# See:
+# * Single View Metrology, Criminisi et al. (1999)
+
+
+def calibrate():
+    pass
