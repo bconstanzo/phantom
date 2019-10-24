7d6eb22c23a35bfa78d10d044f78fd01aafd6a25
bconstanzo <bconstanzo@users.noreply.github.com>
2019-10-24 16:54:30 -0300

    regression: erases a broken implementation of play_list()

--- phantom/video.py
+++ phantom/video.py
@@ -20,19 +20,11 @@ def play_list(images, delay=10, esc_key="q", title="phantom playback"):
     """
     Plays a sequence of images as a video, using cv2.imshow().
 
-    WARNING: as it stands today, it'll crash when used from the console. This is
-    not the intended use case. We'll probably port this to matplotlib in the
-    future.
+    * NOT IMPLEMENTED*
 
     :param images: list of np.ndarray/cv2 images.
     :param delay: how many miliseconds to way for a keypress.
     :param esc_key: (one char string) which keypress will end the playback.
     :param title: title for the playback window
     """
-    images = cycle(images)
-    while True:
-        frame = next(images)
-        cv2.imshow(title, frame)
-        key = cv2.waitKey(delay)
-        if key == ord(esc_key):
-            break
+    pass
