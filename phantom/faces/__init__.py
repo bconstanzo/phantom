0de1ed6d4fbe0784a41f3236530b1a50b413862a
bconstanzo <bconstanzo@users.noreply.github.com>
2020-03-25 13:05:36 -0300

    fix: make normalize_landmark available to the outside world

--- phantom/faces/__init__.py
+++ phantom/faces/__init__.py
@@ -2,7 +2,8 @@ from .faces import (
     # first, the classes
     Shape, Shape5p, Shape68p, Face, Atlas,
     # and now the functions
-    detect, landmark, encode, compare, estimate_age, estimate_gender
+    detect, landmark, encode, compare, estimate_age, estimate_gender,
+    normalize_landmark
     # and not much more for now
 )
 
