5bdee617bb1999bc24b66f41de79a9793d5c84f4
bconstanzo <bconstanzo@users.noreply.github.com>
2022-05-09 19:31:13 -0300

    update: changed the example so that it will run the estimate age/gender models and works as a more generic demo

--- examples/cam_recognize.py
+++ examples/cam_recognize.py
@@ -4,25 +4,17 @@ import cv2
 
 
 from phantom.utils import draw_faces
-from phantom.faces import compare, encode, detect, estimate_age, age_tags
+from phantom.faces import (
+    encode, detect, estimate_age, estimate_gender, age_tags,
+)
 
-known_faces = {
-    "Bruno": "c:/test/phantom/tests/img4.jpg",
-    "Luciano": "c:/test/phantom/tests/luciano2.jpg",
-    "Fer": "c:/test/phantom/tests/fer.jpg",
-    "Ana": "c:/test/phantom/tests/ana.jpg",
-    "Santi": "c:/test/phantom/tests/santi.jpg",
-}
-
-for name, impath in known_faces.items():
-    img = cv2.imread(impath)
-    if img is None:
-        known_faces[name] = None
-        continue
-    known_faces[name] = encode(img)
-    
 
 video = cv2.VideoCapture(0)
+
+def gender_name(x):
+    if gender > 0.2: return "female"
+    if gender < -0.2: return "male"
+    return "not recognized"
 
 while True:
     check, frame = video.read()
@@ -30,17 +22,17 @@ while True:
     if faces:
         encodings = encode(frame, locations=faces)
         for i, e in enumerate(encodings):
-            for k, v in known_faces.items():
-                if compare(e, v) <= 0.6:
-                    age = estimate_age(e)[0]
-                    age_text = age_tags[age]
-                    left, top, right, bottom = faces[i]
-                    x = int (left * 0.5 + right * 0.5)
-                    y = top
-                    cv2.putText(frame, f"{k}", (x+1,y+1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-                    cv2.putText(frame, f"{age_text}", (x-19,y+21), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-                    cv2.putText(frame, f"{k}", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-                    cv2.putText(frame, f"{age_text}", (x-20,y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+            age = estimate_age(e)[0]
+            age_text = age_tags[age]
+            gender = estimate_gender(e)
+            gender_text = gender_name(gender)
+            left, top, right, bottom = faces[i]
+            x = int (left * 0.5 + right * 0.5)
+            y = top
+            cv2.putText(frame, f"{age_text}", (x-19,y+21), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+            cv2.putText(frame, f"{age_text}", (x-20,y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+            cv2.putText(frame, f"{gender_text}", (x-19,y+41), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+            cv2.putText(frame, f"{gender_text}", (x-20,y+40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
     cv2.imshow("Caras", frame)
     key = cv2.waitKey(1)
     if key == ord("q"):
