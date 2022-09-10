f48908920b9fe9282043015f8dd85b1c1160ec84
bconstanzo <bconstanzo@users.noreply.github.com>
2022-09-09 21:56:40 -0300

    fix: change a few issues on the enhancements.py example

--- examples/enhancements.py
+++ examples/enhancements.py
@@ -13,23 +13,23 @@ from phantom.enhance import lucy_richardson_deconv
 
 # example
 # sigmaG: sigma of point spread function
-sigma = 6.0
+sigma = 3.0
 winSize = 8 * sigma + 1
 
-img = cv2.imread('lena.png', cv2.IMREAD_GRAYSCALE)    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.imread('lena.png')    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow('original', img)
 
-img = np.float64(img)   # convert to -> img.convertTo(img, CV_64F);
+#img = np.float64(img)   # convert to -> img.convertTo(img, CV_64F);
 
 # Blur the original image
 img = cv2.GaussianBlur(img, (int(winSize), int(winSize)), sigma, sigma)
 # en C++ ->GaussianBlur(img_origen, img_destino, Size(winSize,winSize), sigmaG, sigmaG )
 
-cv2.normalize(img, img, 0, 1, cv2.NORM_MINMAX)
+#cv2.normalize(img, img, 0, 1, cv2.NORM_MINMAX)
 cv2.imshow('original_blur', img)
 
-result = lucy_richardson_deconv(img, 200, sigma)
-cv2.normalize(result, result, 0, 1, cv2.NORM_MINMAX)
+result = lucy_richardson_deconv(img, 100, sigma)
+# cv2.normalize(result, result, 0, 1, cv2.NORM_MINMAX)
 
 cv2.imshow('result', result)
 cv2.waitKey(0)
